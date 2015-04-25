1eb9dfd24cbfe99ce9c17a4dc9f679fe53f71e82
Emma Kuo <emma.c.kuo@gmail.com>
2015-04-25 14:24:52 -0700

    usage, arg parsing, output directory

--- Xi2Wav/Program.cs
+++ Xi2Wav/Program.cs
@@ -9,26 +9,47 @@ namespace Xi2Wav
 {
     class Program
     {
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("xi2wav infile.xi [outfile.wav]");
+            Console.WriteLine("xi2wav /r indirectory [outdirectory]");
+        }
+
         static void Main(string[] args)
         {
-            if (Directory.Exists(args[0]))
+            var parms = args.Where(a => !a.StartsWith("/")).ToArray();
+            var switches = args.Where(a => a.StartsWith("/")).ToArray();
+            if (switches.Length == 1 && switches[0] == "/r" && parms.Length > 0)
             {
-                foreach (var xiPath in Directory.EnumerateFiles(args[0], "*.xi", SearchOption.AllDirectories))
+                foreach (var xiPath in Directory.EnumerateFiles(parms[0], "*.xi", SearchOption.AllDirectories))
                 {
-                    Convert(xiPath);
+                    var wavPath = Path.GetDirectoryName(xiPath) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(xiPath) + ".wav";
+                    if (parms.Length >= 2)
+                        wavPath = parms[1] + Path.DirectorySeparatorChar + wavPath;
+                    Directory.CreateDirectory(Path.GetDirectoryName(wavPath));
+                    Convert(xiPath, wavPath);
                 }
+            }
+            else if (switches.Length == 0 && parms.Length > 0)
+            {
+                var xiPath = parms[0];
+                string wavPath;
+                if (parms.Length >= 2)
+                    wavPath = parms[1];
+                else
+                    wavPath = Path.GetDirectoryName(xiPath) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(xiPath) + ".wav";
+                Convert(xiPath, wavPath);
             }
             else
             {
-                Convert(args[0]);
+                Usage();
             }
-
         }
 
-        static void Convert(string xiPath)
+        static void Convert(string xiPath, string wavPath)
         {
             Console.WriteLine("Converting {0}", xiPath);
-            var wavPath = Path.GetDirectoryName(xiPath) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(xiPath) + ".wav";
             using (var instream = new FileStream(xiPath, FileMode.Open))
             using (var outstream = new FileStream(wavPath, FileMode.Create))
             {
