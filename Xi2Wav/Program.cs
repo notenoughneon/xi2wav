de6cba63bbaa5f581a01fe404226b5644ad4e9c9
Emma Kuo <emma.c.kuo@gmail.com>
2015-04-24 22:47:23 -0700

    bulk file conversion

--- Xi2Wav/Program.cs
+++ Xi2Wav/Program.cs
@@ -11,15 +11,31 @@ namespace Xi2Wav
     {
         static void Main(string[] args)
         {
-            var xiPath = args[0];
-            var wavPath = Path.GetFileNameWithoutExtension(xiPath) + ".wav";
+            if (Directory.Exists(args[0]))
+            {
+                foreach (var xiPath in Directory.EnumerateFiles(args[0], "*.xi", SearchOption.AllDirectories))
+                {
+                    Convert(xiPath);
+                }
+            }
+            else
+            {
+                Convert(args[0]);
+            }
+
+        }
+
+        static void Convert(string xiPath)
+        {
+            Console.WriteLine("Converting {0}", xiPath);
+            var wavPath = Path.GetDirectoryName(xiPath) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(xiPath) + ".wav";
             using (var instream = new FileStream(xiPath, FileMode.Open))
             using (var outstream = new FileStream(wavPath, FileMode.Create))
             {
                 var instrument = new XiInstrument(instream);
                 if (instrument.Samples.Count == 0)
                     throw new Exception("Instrument has no samples");
-                if (instrument.Samples.Count > 1)
+                if (instrument.Samples.Count > 2)
                     throw new Exception("Multi-samples not yet supported");
                 if (instrument.Samples.Any(s => !s.Is16Bit))
                     throw new Exception("8 bit samples not yet supported");
